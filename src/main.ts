06ed01041a2a7ba6b0332acfd8a2cd446dd5e507
旧日丨四季列车 <Nostalgia-Season-Train@outlook.com>
2026-04-29 10:52:29 +0800

    代码 改进：优化导入路径

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { Plugin } from 'obsidian'
-import { openDesktopWindow } from './feature/desktop'
+import { openDesktopWindow } from 'feature/desktop'
 
 export default class DesktopNotePlugin extends Plugin {
   async onload() {
